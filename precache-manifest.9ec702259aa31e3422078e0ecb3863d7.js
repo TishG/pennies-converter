cb34b45b7d6f88de3a17b771dac28b0ac3d5859a
Tish <latisha.griffiths@programmer.net>
2019-06-01 09:39:20 -0500

    Updates

---
+++ precache-manifest.9ec702259aa31e3422078e0ecb3863d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a038c04f8d96ae1b0c4c19e06ef31a40",
+    "url": "/pennies-converter/index.html"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/pennies-converter/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "36687b8f323cbce00bff",
+    "url": "/pennies-converter/static/js/main.f2afc7c3.chunk.js"
+  },
+  {
+    "revision": "5f2b7e019fc7d009f181",
+    "url": "/pennies-converter/static/js/runtime~main.2ae67193.js"
+  }
+]);
